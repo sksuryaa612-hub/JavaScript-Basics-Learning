ba53fd2e0982eeedf5ca3cdc921ab26f7e7db97a
Suryaa <sksuryaa612@gmail.com>
2026-05-22 20:20:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+// Calculator Logic
+
+// let num1 = 10;
+// let num2 = 0;
+
+// let operator = ""; // Change this to "+", "-", "*", or "/" to test different operations
+
+// if (operator === "+") {
+//     console.log(num1 + num2);
+// } else if (operator === "-") {
+//     console.log(num1 - num2);
+// } else if (operator === "*") {
+//     console.log(num1 * num2);
+// } else if (operator === "/") {
+//     if (num2 !== 0) {
+//         console.log(num1 / num2);
+//     } else {
+//         console.log(" num2 cannot be zero for division");
+//     }
+// } else {
+//      console.log("Invalid operator");
+// }
+
+// Grade Calculation Logic
+
+// let marks = 94;
+
+// let bonus = 5;
+
+// marks += bonus;
+
+// let grade;
+
+// // If marks are above 100
+// if (marks > 100) {
+//     grade = "INVALID";
+// }
+// else if (marks >= 90) {
+//     grade = "GRADE A";
+// }
+// else if (marks >= 70) {
+//     grade = "GRADE B";
+// }
+// else if (marks >= 50) {
+//     grade = "GRADE C";
+// }
+// else {
+//     grade = "GRADE F";
+// }
+
+// let result = (marks >= 50) ? "Pass" : "Fail";
+
+// let remark;
+
+// switch (grade) {
+//     case "GRADE A":
+//         remark = "Excellent!";
+//         break;
+
+//     case "GRADE B":
+//         remark = "Good";
+//         break;
+
+//     case "GRADE C":
+//         remark = "Average";
+//         break;
+
+//     case "GRADE F":
+//         remark = "Needs Improvement";
+//         break;
+
+//     default:
+//         remark = "Invalid Grade";
+// }
+
+// console.log("Marks:", marks);
+// console.log("Grade:", grade);
+// console.log("Result:", result);
+// console.log("Remark:", remark);
